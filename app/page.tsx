52afea23a5acc1241d97fbffa3dcc4145407d79a
Minutzu321 <44315313+Minutzu321@users.noreply.github.com>
2023-03-22 00:51:11 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,56 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Scena from '@/components/Scena'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  <main>
+    <section>
+      <div className="root-page-container">
+        <div className="terminal-outer-container">
+          <div className="terminal-inner-container">
+            <div className="terminal-title-bar">
+              <div className="terminal-title-actions">
+                <div className="title-btn title-btn__red"></div>
+                <div className="title-btn title-btn__yellow"></div>
+                <div className="title-btn title-btn__green"></div>
+              </div>
+  
+              <div className="terminal-title-text">terminal</div>
+            </div>
+  
+            <div className="terminal-content-container">
+              <div className="terminal-content-text">
+                <p>
+                  <span className="yellow">ub@can</span>:<span className="blue">~</span>$
+                    <span className="cursor">
+                      <span id="consolaMesaj"></span>
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div className="terminal-mode-line-container">
+              <div className="terminal-file-display">ub_can.sh</div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+    </section>
+    <section>
+      <div className="root-page-container">
+        <div className="terminal-outer-container">
+          <div className="terminal-inner-container">
+            <div className="terminal-title-bar">
+              <div className="terminal-title-text">Randare 3D</div>
+            </div>
+            
+            <div className="terminal-content-container cansat">
+                <Scena/>
+            </div>
+          </div>
         </div>
       </div>
+    </section>
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    
+  </main>
   )
 }
